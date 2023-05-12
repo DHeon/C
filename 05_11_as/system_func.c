6b43d49637c8608ee8c79504d83aafb35234ea93
HEON <47468208+DHeon@users.noreply.github.com>
2023-05-12 11:12:40 +0900

    Create system_func.c

---
+++ 05_11_as/system_func.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    system("dir"); //system 함수는 명령 프롬프트의 명령을 실행시키는 함수이다.
+    //dir은 경로의 파일과 폴더의 목록이 뜬다.
+    
+    system("cls");
+    //cls는 출력 화면을 지운다.
+    return 0;
+}
