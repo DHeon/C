051966aa417e00e3270f7c68c9456920c2f2cef0
HEON <47468208+DHeon@users.noreply.github.com>
2023-05-13 13:43:31 +0900

    Update tree.c

--- 05_11_as/tree.c
+++ 05_11_as/tree.c
@@ -17,7 +17,7 @@ int main(){
     rad = deg * (M_PI / 180.0);
     tree = tan(rad) *distance + height;
     //탄젠트 * 밑변을 하면 높이가 나오므로
-    //각도를 라디안으로 바꿔서 tan rad 에 distance를 구하면
+    //각도를 라디안으로 바꿔서 tan(rad) 에 distance를 곱하면
     //측정자의 눈높이부터에서의 나무높이가 구해진다. 
     //거기에 측정자의 눈높이까지의 높이를 구하면 완전한 나무의 
     //높이를 구할 수 있다. 
